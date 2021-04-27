092bbca855221094b708a2fc02ddb8feec995860
aimadelkhattabi <aimadkha@gmail.com>
2021-04-27 14:28:21 +0000

    add animation

--- vue_portfolio/src/components/AboutPage.vue
+++ vue_portfolio/src/components/AboutPage.vue
@@ -17,7 +17,7 @@
       <div class="row">
         <div class="about__content col-md-8">
           <h5 class="text-uppercase font-weight-bold text-md-left">who am i</h5>
-          <p class="about__text text-justify">
+          <p data-aos="zoom-in-up" class="about__text text-justify">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
             architecto commodi nam, expedita voluptatem ipsum. Corrupti
             asperiores quasi recusandae amet, rem expedita a assumenda sequi
@@ -25,7 +25,7 @@
             quia? Placeat perferendis alias, earum voluptate officia assumenda.
             Laudantium.
             <br />
-            <span class="text-dark">
+            <span data-aos="zoom-out-up" class="text-dark">
               voluptatem quas ullam molestiae? Laborum facere omnis suscipit
               temporibus corrupti cupiditate cum labore placeat quia, voluptatum
               ipsa eaque doloremque quas saepe expedita voluptates. Earum optio
@@ -37,20 +37,21 @@
         </div>
         <div class="about__contact w-100 col-md-4">
           <h5 class="text-uppercase font-weight-bold text-center">
-            aimad elkhattabi
+            Hugo Bollon
           </h5>
           <p class="font-weight-bold">
-            E-mail: <span class="font-weight-normal">aimadkha@gmail.com</span>
+            E-mail:
+            <span class="font-weight-normal">hugo.bollon@gmail.com</span>
           </p>
           <p class="font-weight-bold">
-            Phone: <span class="font-weight-normal">+212 6 75 20 64 72</span>
+            Phone: <span class="font-weight-normal"> +33 6 51 99 80 78</span>
           </p>
           <p class="font-weight-bold">
-            City: <span class="font-weight-normal">Meknes, Maroc</span>
+            City: <span class="font-weight-normal">Chambéry, France</span>
           </p>
           <p class="font-weight-bold">
             Languages:
-            <span class="font-weight-normal">Arabe, francais, anglais</span>
+            <span class="font-weight-normal">French, English</span>
           </p>
           <div class="about__icons">
             <a class="active"
--- vue_portfolio/src/components/Projects.vue
+++ vue_portfolio/src/components/Projects.vue
@@ -1,19 +1,40 @@
 <template>
   <section id="projects" class="text-white py-5">
-    <vue-timeline-update
-      :date="new Date('2017-02-26')"
-      title="IG_Automation_Bot"
-      description="Python script for Instagram automation using Instaloader. Used for auto following and DM. Easily configurable through Yaml config files!"
-      thumbnail="/images/vuetimeline/initial_d.jpg"
-      category="announcement"
-      icon="code"
-      color="red"
-    />
     <div id="projects__heading" class="container">
       <p class="display-4 mt-5 mb-0">MY PROJECTS</p>
       <span class="mt-0">This is my story</span>
       <hr id="about__line" class="w-25 bg-white" />
     </div>
+    <div class="container">
+      <vue-timeline-update
+        :date="new Date('2017-02-26')"
+        title="v2.2.0 - Initial D"
+        description="Python script for Instagram automation using Instaloader. Used for auto following and DM. Easily configurable through Yaml config files!"
+        thumbnail="https://images.unsplash.com/photo-1542546068979-b6affb46ea8f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+        category="announcement"
+        icon="code"
+        color="red"
+      />
+      <vue-timeline-update
+        :date="new Date('2017-02-26')"
+        title="v2.2.0 - Initial D"
+        description="Python script for Instagram automation using Instaloader. Used for auto following and DM. Easily configurable through Yaml config files!"
+        thumbnail="https://images.unsplash.com/photo-1542546068979-b6affb46ea8f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+        category="announcement"
+        icon="code"
+        color="red"
+      />
+      <vue-timeline-update
+        :date="new Date('2017-02-26')"
+        title="v2.2.0 - Initial D"
+        description="Python script for Instagram automation using Instaloader. Used for auto following and DM. Easily configurable through Yaml config files!"
+        thumbnail="https://images.unsplash.com/photo-1505238680356-667803448bb6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fGNvZGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"
+        category="announcement"
+        icon="code"
+        color="green"
+      />
+    </div>
+
     <div id="project__card" v-for="project in projects" :key="project.name">
       <h4 data-aos="zoom-in-left">{{ project.name }}</h4>
       <img data-aos="zoom-in-right" :src="project.img" alt="" class="w-50" />
